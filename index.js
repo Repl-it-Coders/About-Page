23384fbe7d36d8c92905759f020110aea2cf5590
BD103 <bdeep10355@gmail.com>
2021-02-06 13:34:28 +0000

    Many changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const express = require('express');
 const app = express();
 const visits = 0
@@ -8,15 +10,27 @@ app.use(express.static('Static'));
 app.get('/', (req, res) => {
   res.render('index');
   // Just decided to change this:
-  console.log("[DEBUUG CHANTER] WE ON YEM BOUT PAGE BOI");
+  console.log("Serving Home Page");
 });
 app.get('/signup', (req, res) => {
   res.render('signup');
-  console.log("DIS DEM SIGNUP PAGE BOI");
+  console.log("Serving Signup Page");
 });
-// Don't get rid of anything below!
 
-app.get('/ch1ck3n', (req, res) => {
+// Don't get rid of anything below! (Except the inefficient use of all those request handlers)
+
+app.get("/:user", (req, res) => {
+	fs.exists(`views/${req.params.user}.html`, (exists) => {
+		if (exists)
+			res.render(req.params.user);
+      console.log("Serving user: " + req.params.user);
+		else
+			res.status(404).render("404");
+      console.log("404 Error, Page Not Found")
+	});
+});
+
+/*app.get('/ch1ck3n', (req, res) => {
 
   res.render('ch1ck3n');
   console.log("Entering, the one, the only, ch1ck3n!");
@@ -55,72 +69,18 @@ app.get("/head", (req, res) => {
   res.render("head");
   console.log("Entering a floating head's territory")
 });
-app.get("/EPIC", (req, res) => {
+app.get("/epicraisin", (req, res) => {
   res.render("epicraisin");
-  console.log("SOUR!!! RASINSS!!");
+  console.log("beep beep epicraisin");
 });
 app.get('/programmeruser', (req, res) => {
-	res.render('programmeruser');
-});
+	res.redirect('https://programmeruser.repl.co');
+});*/
 
-app.get("*", (req, res) => {
- 
-  res.render("404");
-  console.log("Hi, you reached the wrong number. Please try again IMMEDIATELY");
-});
-// DON'T LOOK PAST HERE PUBLIC
-// Devs i need help please make this work
-app.get("/admin", (req, res) => {
-  res.render('admin')
-  console.log("HOW DID YOU FIND THIS HACKER")
-});
+// app.use((req, res, next) => {res.status(404).send("Error")});
 
-// Random tings
-app.get("/a", (req, res) => {
-  console.log("a");
-});
-app.get("/b", (req, res) => {
-  console.log("b");
-});
-app.get("/c", (req, res) => {
-  console.log("c");
-});
-app.get("/d", (req, res) => {
-  console.log("d");
-});
-app.get("/e", (req, res) => {
-  console.log("e");
-});
-app.get("/f", (req, res) => {
-  console.log("f");
-});
-app.get("/g", (req, res) => {
-  console.log("g");
-});
-app.get("/h", (req, res) => {
-  console.log("h");
-});
-app.get("/i", (req, res) => {
-  console.log("/i");
-});
-app.get("/j", (req, res) => {
-  console.log("j");
-});
-app.get("/k", (req, res) => {
-  console.log("k");
-});
-app.get("/l", (req, res) => {
-  console.log("l");
-});
-app.get("/m", (req, res) => {
-  console.log("m");
-});
-app.get("/n", (req, res) => {
-  console.log("n");
-});
+// app.use((req, res, next) => {res.status(404).render('404')});
 
-app.use((req, res, next) => {res.status(404).render('404')});
-
-app.listen(8080, () => {
-  console.log('Page iiiis woooorking');
+app.listen(8000, () => {
+  console.log("Server running.");
 });
